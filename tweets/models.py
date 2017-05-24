fd52b89667f24444970ea2f8081c5652af5298d9
bharris62 <blakebharris@gmail.com>
2017-05-23 23:06:41 -0500

    building out views, get all tweets and 1 tweet

--- tweets/models.py
+++ tweets/models.py
@@ -1,8 +1,14 @@
+from django.conf import settings
 from django.db import models
-
 # Create your models here.
 
 class Tweet(models.Model):
-    content = models.TextField()
-    content2 = models.TextField()
-    content3 = models.TextField()
+
+    user = models.ForeignKey(settings.AUTH_USER_MODEL)
+    content = models.CharField(max_length=140)
+    timestamp = models.TimeField(auto_now=True)
+    updated = models.TimeField(auto_now=True, )
+
+
+    def __str__(self):
+        return str(self.content)
