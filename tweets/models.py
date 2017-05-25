6baf020bd35457a59931c729d94cb69595a46d64
bharris62 <blakebharris@gmail.com>
2017-05-24 22:38:18 -0500

    updated

--- tweets/models.py
+++ tweets/models.py
@@ -6,8 +6,8 @@ class Tweet(models.Model):
 
     user = models.ForeignKey(settings.AUTH_USER_MODEL)
     content = models.CharField(max_length=140)
-    timestamp = models.TimeField(auto_now=True)
-    updated = models.TimeField(auto_now=True, )
+    timestamp = models.DateTimeField(auto_now=True)
+    updated = models.DateTimeField(auto_now=True, )
 
 
     def __str__(self):
--- tweets/views.py
+++ tweets/views.py
@@ -1,20 +1,32 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 from .models import Tweet
+
+class TweetDetailView(DetailView):
+    template_name = "tweets/detail_view.html"
+    queryset = Tweet.objects.all()
+
+
+class TweetListView(ListView):
+    template_name = "tweets/list_view.html"
+    queryset = Tweet.objects.all()
+
+
 # Create your views here.
-def tweet_detail_view(request, id=1):
-    obj = Tweet.objects.get(id=id)
-    print(obj)
-    context = {
-        'object': obj
-    }
-    return render(request, "tweets/detail_view.html", context)
-
-
-def tweet_list_view(request, id=1):
-    query_set = Tweet.objects.all()
-    print(query_set)
-
-    context = {
-        'object_list': query_set
-    }
-    return render(request, "tweets/list_view.html", context)
+# def tweet_detail_view(request, id=1):
+#     obj = Tweet.objects.get(id=id)
+#     print(obj)
+#     context = {
+#         'object': obj
+#     }
+#     return render(request, "tweets/detail_view.html", context)
+#
+#
+# def tweet_list_view(request, id=1):
+#     query_set = Tweet.objects.all()
+#     print(query_set)
+#
+#     context = {
+#         'object_list': query_set
+#     }
+#     return render(request, "tweets/list_view.html", context)
